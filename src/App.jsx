b366b65dcc6c8767ac270d40b385c66bad1f6df0
anton lokianov <antonlokianov@gmail.com>
2023-09-06 14:55:59 +0300

    adding suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 const TopLeftImage = lazy(() => import("./components/TopLeftImage"));
 const Navbar = lazy(() => import("./components/Navbar"));
 const Header = lazy(() => import("./components/Header"));
@@ -6,10 +6,24 @@ const Home = lazy(() => import("./components/Home"));
 const About = lazy(() => import("./components/About"));
 const Projects = lazy(() => import("./components/Projects"));
 const Contact = lazy(() => import("./components/Contact"));
+import { motion } from "framer-motion";
+import { fadeIn } from "./utils/variants";
 
 const App = () => {
   return (
-    <>
+    <Suspense
+      fallback={
+        <div className="h-screen flex items-center justify-center">
+          <motion.div
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="text-accent text-4xl font-bold">
+            Loading...
+          </motion.div>
+        </div>
+      }>
       <div id="home" className="h-screen">
         <TopLeftImage />
         <Navbar />
@@ -31,7 +45,7 @@ const App = () => {
         className="h-screen bg-site bg-cover bg-no-repeat relative w-full">
         <Contact />
       </div>
-    </>
+    </Suspense>
   );
 };
 
