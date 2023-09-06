08edb38549e1255847477084f329f2da2e8e2220
anton-lokinanov <antonlokianov@gmail.com>
2023-09-07 01:21:51 +0300

    adding projects image

--- src/components/ProjectSlider.jsx
+++ src/components/ProjectSlider.jsx
@@ -11,19 +11,19 @@ const projectSlider = {
       images: [
         {
           title: "title",
-          path: "",
+          path: "/storeproject.png",
         },
         {
           title: "title",
-          path: "",
+          path: "/mediaproject.png",
         },
         {
           title: "title",
-          path: "",
+          path: "/cryptoproject.png",
         },
         {
           title: "title",
-          path: "",
+          path: "/vacationproject.png",
         },
       ],
     },
@@ -58,16 +58,16 @@ const ProjectSlider = () => {
       modules={[Pagination]}
       className="h-[280px] sm:h-[480px]">
       {projectSlider.slides.map((slide, index) => (
-        <SwiperSlide key={index}>
-          <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+        <SwiperSlide key={index} className="h-full">
+          <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer h-full">
             {slide.images.map((image, index) => (
               <div
                 key={index}
-                className="relative rounded-lg overflow-hidden flex items-center justify-center group">
-                <div className="flex items-center justify-center relative overflow-hidden group">
-                  <img src={image.path} alt="" width={500} height={300} />
+                className="relative flex rounded-lg overflow-hidden items-center justify-center group h-full">
+                <div className="flex items-center justify-center relative w-full h-full overflow-hidden group">
+                  <img src={image.path} alt="" className="object-fit w-full h-full" />
                   <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
-                  <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl-translate-y-20 transition-all duration-300">
+                  <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full group-hover:-translate-y-10 group-hover:xl-translate-y-20 transition-all duration-300">
                     <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
                       <div className="delay-100">title part 1</div>
                       <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
@@ -87,5 +87,6 @@ const ProjectSlider = () => {
     </Swiper>
   );
 };
+
 
 export default ProjectSlider;
