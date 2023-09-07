03cf97b56a844b32d72039ed3af1e1e04a1698b6
anton-lokinanov <antonlokianov@gmail.com>
2023-09-07 03:53:57 +0300

    adding title for projects

--- src/components/ProjectSlider.jsx
+++ src/components/ProjectSlider.jsx
@@ -10,19 +10,19 @@ const projectSlider = {
     {
       images: [
         {
-          title: "title",
+          title: "MERN store project",
           path: "/storeproject.png",
         },
         {
-          title: "title",
+          title: "MERN media project",
           path: "/mediaproject.png",
         },
         {
-          title: "title",
+          title: "crypto api project",
           path: "/cryptoproject.png",
         },
         {
-          title: "title",
+          title: "Fullstack vacation project",
           path: "/vacationproject.png",
         },
       ],
@@ -30,19 +30,19 @@ const projectSlider = {
     {
       images: [
         {
-          title: "title",
+          title: "Coming soon",
           path: "",
         },
         {
-          title: "title",
+          title: "Coming soon",
           path: "",
         },
         {
-          title: "title",
+          title: "Coming soon",
           path: "",
         },
         {
-          title: "title",
+          title: "Coming soon",
           path: "",
         },
       ],
@@ -69,12 +69,12 @@ const ProjectSlider = () => {
                   <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                   <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full group-hover:-translate-y-10 group-hover:xl-translate-y-20 transition-all duration-300">
                     <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-                      <div className="delay-100">title part 1</div>
+                      <div className="delay-100">{image.title}</div>
                       <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                        title part 2
+                        review code
                       </div>
                       <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
-                        <BsArrowRight />
+                        <BsArrowRight  />
                       </div>
                     </div>
                   </div>
