80eef0a607b5b3ba1ac9ca164d7666d444bebaef
anton-lokinanov <antonlokianov@gmail.com>
2023-12-03 16:50:41 +0200

    changing the navlink click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,11 +45,17 @@ const Navbar = () => {
     return () => observer.disconnect();
   }, []);
 
+  const handleNavClick = (e, path) => {
+    e.preventDefault();
+    window.location.replace(path);
+  };
+
   return (
     <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-30 top-0  w-full xl:w-16 xl:max-w-md xl:h-screen">
       <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {navData.map((navLink, index) => (
           <a
+            onClick={(e) => handleNavClick(e, navLink.path)}
             href={navLink.path}
             className={`${
               navLink.path === currentHash ? "text-accent" : ""
